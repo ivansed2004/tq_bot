03cad1a000f6e00bdf0659b696a39945b9da12bc
ivan <id2like0to0know4@gmail.com>
2026-02-15 13:33:10 +0300

    typo fixed

--- src/utils.py
+++ src/utils.py
@@ -34,7 +34,7 @@ def get_queue_info_keyboard(start_time, week_day, action_name):
                 KeyboardButton(f"🔙{action_name}"),
                 KeyboardButton("🏛В меню")
             ]
-            ]
+        ]
     else:
         return [
             [   
