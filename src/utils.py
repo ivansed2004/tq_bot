1aeaef6237d53ec0ac75558d3e3f4c04f94477a5
ivan <id2like0to0know4@gmail.com>
2026-03-13 00:51:12 +0300

    1. discipline_info() and queue_info() methods added to display Discipline or Queue information; 2. using api calls instead of direct queries; 3. functions that use api calls are declared as async

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 from telegram import KeyboardButton
+from main import BACKEND_URL
+from api import api_remote_call
 
 
 def get_menu_keyboard():
@@ -14,8 +16,8 @@ def get_menu_keyboard():
     ]
 
 
-def get_queue_info_keyboard(start_time, week_day, action_name):
-    state = get_queue_state(start_time, week_day)
+async def get_queue_info_keyboard(start_time, week_day, action_name):
+    state = await api_remote_call(f"{BACKEND_URL}/queue/state?start_time={start_time}&week_day={week_day}")
     if ( state.value == "Открытая" ):
         return [
             [ KeyboardButton("⬆️Встать") ],
@@ -43,20 +45,23 @@ def get_queue_info_keyboard(start_time, week_day, action_name):
         ]
 
 
-def get_listed_queues_keyboard(regime: str, action_name: str):
-    keyboard = [ [KeyboardButton(f"{action_name}   '{q.discipline.name}'   {q.week_day}   {q.start_time.strftime('%H:%M')}")] for q in get_queues(regime) ]
+async def get_listed_queues_keyboard(regime: str, action_name: str):
+    queues = await api_remote_call(f"{BACKEND_URL}/queues?regime={regime}")
+    keyboard = [ [KeyboardButton(f"{action_name}   '{q.discipline.name}'   {q.week_day}   {q.start_time.strftime('%H:%M')}")] for q in queues ]
     keyboard.append( [KeyboardButton("🏛В меню")] )
     return keyboard
 
 
-def get_users_subscribed_queues_keyboard(tid):
-    keyboard = [ [KeyboardButton(f"Открыть   '{q.discipline.name}'   {q.week_day}   {q.start_time.strftime('%H:%M')}")] for q in get_subscribed_queues_by_user(tid) ]
+async def get_users_subscribed_queues_keyboard(tid):
+    queues = await api_remote_call(f"{BACKEND_URL}/user/queues?tid={tid}")
+    keyboard = [ [KeyboardButton(f"Открыть   '{q.discipline.name}'   {q.week_day}   {q.start_time.strftime('%H:%M')}")] for q in queues ]
     keyboard.append( [KeyboardButton("🏛В меню")] )
     return keyboard
 
 
-def get_listed_disciplines_keyboard():
-    keyboard = [ [KeyboardButton(f"'{d.name}'   {d.start_time.strftime('%H:%M')}-{d.end_time.strftime('%H:%M')}   {d.date.strftime('%d.%m')}")] for d in get_all_disciplines() ]  
+async def get_listed_disciplines_keyboard():
+    disciplines = await api_remote_call(f"{BACKEND_URL}/discipline/all")
+    keyboard = [ [KeyboardButton(f"'{d.name}'   {d.start_time.strftime('%H:%M')}-{d.end_time.strftime('%H:%M')}   {d.date.strftime('%d.%m')}")] for d in disciplines ]  
     keyboard.append( [KeyboardButton("🏛В меню")] )
     return keyboard
 
@@ -70,3 +75,29 @@ def get_discipline_info_keyboard():
             KeyboardButton("🏛В меню")
         ]
     ]
+
+
+def discipline_info(discipline):
+    return (
+        f"Порядковый номер: {discipline.number}\n"
+        f"Время начала: {discipline.start_time.strftime('%H:%M')}\n"
+        f"Время окончания: {discipline.end_time.strftime('%H:%M')}\n"
+        f"Наименование: {discipline.name}\n"
+        f"Дата: {discipline.week_day}, {discipline.date.strftime('%d.%m')}\n"
+        f"Преподаватель: {discipline.teacher}\n"
+        f"Аудитория: {discipline.location}\n"
+        f"Тип: {discipline.type}"
+    )
+
+
+def queue_info(queue):
+    queue_positioned = [ f"{i+1}. {user.fullname}" for i, user in enumerate(queue.content) ]
+    return "\n".join((
+        f"Оповещение о начале каждые: {queue.week_day}, {queue.start_time.strftime('%H:%M')}",
+        f"Статус: {queue.state.value}",
+        f"Размер: {queue.size}",
+        f"\n\n ~== ОБЪЕКТ ПЛАНИРОВАНИЯ ==~ \n\n"
+        f"{queue.discipline}",
+        "Список пользователей:",
+        "\n".join(queue_positioned)
+    ))
